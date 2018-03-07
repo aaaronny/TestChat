72a637782208cf24edea21b404a18b2937169529
Alessandro Apicella <alexapicella00@gmail.com>
2018-03-07 18:10:33 +0100

    start reduct code

--- src/main/resources/static/reactjs/reactEngine.js
+++ src/main/resources/static/reactjs/reactEngine.js
@@ -4,7 +4,32 @@ function onSignInGoogle(googleUser) {
 	  username = profile.getEmail();
 	  displayName = profile.getName();
 	  profileImg = profile.getImageUrl();
-	  setOnChatDiv();
-	  connect();
-	  ReactDOM.render(<ChatFull client={client} username={username} display={displayName} />, document.getElementById('react'));
+	  preLoad();
+}
+
+function login() {
+	
+	var utente = { username: $('#user').val(), password: $('#pass').val() };
+
+	$.ajax({
+		type : "POST",
+		url : '/login',
+		dataType : 'json',
+		contentType : 'application/json',
+		data : JSON.stringify(utente),
+		statusCode : {
+			200 : function(res) {
+				username = $('#user').val();				
+				preLoad();
+			}
+		}
+	});
+
+}
+
+function preLoad(){
+	$('#loginBox').css("display", "none");
+	$('#chatBox').css("display", "inline-block");
+	client = Stomp.over(new SockJS('/tsch'));
+	ReactDOM.render(<ChatFull client={client} username={username} display={displayName} />, document.getElementById('react'));
 }
