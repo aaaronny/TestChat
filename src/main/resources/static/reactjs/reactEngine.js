5c6349875ef793c2c5c40a7545edfb6c830b42b6
alessandroapicella <alessandro.apicella@stepsrl.org>
2018-03-08 19:40:46 +0100

    start accountkit

--- src/main/resources/static/reactjs/reactEngine.js
+++ src/main/resources/static/reactjs/reactEngine.js
@@ -41,3 +41,22 @@ function preLoad(){
 	client = Stomp.over(new SockJS('/tsch'));
 	ReactDOM.render(<ChatFull client={client} username={username} display={displayName} />, document.getElementById('chatBox'));
 }
+
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1022508357842402',
+    xfbml      : true,
+    version    : 'v2.12'
+  });
+  FB.AppEvents.logPageView();
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
