81602fbd5d8ca279b1b8e7746125298e43f27ff8
alessandroapicella <alessandro.apicella@stepsrl.org>
2018-03-08 21:59:47 +0100

    login implements pt1

---
+++ src/main/java/aaaronny/dev/testchat/controller/AccountKitController.java
@@ -0,0 +1,23 @@
+package aaaronny.dev.testchat.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class AccountKitController {
+	
+	private static final Logger logger = Logger.getLogger(AccountKitController.class);
+
+    @RequestMapping("/requestToken")
+    public String requestToken(@RequestParam("code") String code) {
+    	String url = "https://graph.accountkit.com/v1.3/access_token?grant_type=authorization_code&code=";
+    	url += code + "&access_token=AA|1022508357842402|fe80861d44e55b2054546bcaf64deb4c";
+		RestTemplate restTemplate = new RestTemplate();
+		String res = restTemplate.getForObject(url, String.class);
+		logger.info("ACCOUNT KIT JSON RESULT TOKEN >>> " + res);
+        return "/";
+    }
+}
