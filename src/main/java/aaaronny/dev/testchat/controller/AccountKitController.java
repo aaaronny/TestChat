7eefba14994ab0efc03948f08f5e9250574568b0
Alessandro Apicella <alexapicella00@gmail.com>
2018-03-09 16:50:40 +0100

    fix for account kit

--- src/main/java/aaaronny/dev/testchat/controller/AccountKitController.java
+++ src/main/java/aaaronny/dev/testchat/controller/AccountKitController.java
@@ -13,24 +13,20 @@ public class AccountKitController {
 	private static final Logger logger = Logger.getLogger(AccountKitController.class);
 
     @RequestMapping("/requestToken")
-    public String requestToken(@RequestParam("code") String code) {
+    public String requestToken(@RequestParam("code") String code, ModelMap model) {
     	String url = "https://graph.accountkit.com/v1.1/access_token?grant_type=authorization_code&code=";
     	url += code + "&access_token=AA|1022508357842402|a9ad38d68d09a9f2e0e3eb8b3bf9cffb";
 		logger.info("ACCOUNT KIT JSON RESULT TOKEN >>> " + url);
 		RestTemplate restTemplate = new RestTemplate();
 		String res = restTemplate.getForObject(url, String.class);
-		logger.info("ACCOUNT KIT JSON RESULT TOKEN >>> " + res.toString());
-        return "testpage";
+		logger.info("ACCOUNT KIT JSON RESULT TOKEN >>> " + res);
+    	model.addAttribute("akResponse", res);
+        return "testtoken";
     }
     
     @RequestMapping("/falder")
     public String testpage(ModelMap model) {
-    	model.addAttribute("ciucci", "Hi my name is Ciucci!");
+    	model.addAttribute("akResponse", "FALDER JOLTER JOYASS JORAIS FORAIS BLACKBOARD OSSIGENO");
         return "testtoken";
-    }
-    
-    @RequestMapping("/jolter")
-    public String testpage() {
-        return "testpage";
     }
 }
