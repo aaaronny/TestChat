912c1f128961417ddad15225fb05f0111826549a
Alessandro Apicella <alexapicella00@gmail.com>
2018-03-09 12:09:41 +0100

    fix form account kit bug, new style of bubble message

--- src/main/resources/static/reactjs/chatMessage.js
+++ src/main/resources/static/reactjs/chatMessage.js
@@ -4,9 +4,13 @@ class ChatMessage extends React.Component {
 	}
 
 	render() {
+		let bubble = 'speech-bubble bubble-' + this.props.isMy;
+		let mex = this.props.content;
+		if (mex == '' || mex == null)
+			mex = '...';
 		return (
-				<div className={this.props.isMy}><div className="message">
-				<p className="sender">{this.props.display}</p><p className="textBody">{this.props.content}</p>
+				<div className={this.props.isMy}><div className={bubble}>
+				<p className="sender">{this.props.display}</p><p className="textBody">{mex}</p>
 				<p className="time">{this.props.date}</p></div></div>
 		);
 	}
